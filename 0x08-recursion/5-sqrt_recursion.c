901b108ec2b9a3e5b01c21ea0d56748104c089a8
Diana Nnaemeka <104990762+dianadeycode@users.noreply.github.com>
2022-06-22 15:17:07 +0100

    Updated Task 5: Square Root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,10 +8,10 @@ int _sqrt(int prev, int root);
  */
 int _sqrt_recursion(int n)
 {
-					if (n < 0)
-									return (-1);
+	if (n < 0)
+		return (-1);
 
-					return (_sqrt(1, n));
+	return (_sqrt(1, n));
 }
 
 /**
@@ -22,9 +22,9 @@ int _sqrt_recursion(int n)
  */
 int _sqrt(int prev, int root)
 {
-				if (prev > root)
-								return (-1);
-				else if (prev * prev == root)
-								return (prev);
-				return (_sqrt(prev + 1, root));
+	if (prev > root)
+		return (-1);
+	else if (prev * prev == root)
+		return (prev);
+	return (_sqrt(prev + 1, root));
 }
