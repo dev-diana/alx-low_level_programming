815238a5d122e5cac3d5ef39d71fd8d8bc0a20c5
Diana Nnaemeka <104990762+dianadeycode@users.noreply.github.com>
2022-07-14 18:06:34 +0100

    Header File

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,6 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*
+ * File: lists.h
+ *
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the current directory.
+ */
+
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,7 +17,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Alx SE project
+ *              for Alx SE project
  */
 typedef struct list_s
 {
@@ -23,4 +32,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* LISTS_H */
